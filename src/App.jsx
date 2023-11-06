b3a021fc61e95d5caafbd33978207241244dfaf2
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-11-06 12:37:13 +0000

    sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "./app.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portolio1</section>
+    <section>Portolio2</section>
+    <section>Portolio3</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
