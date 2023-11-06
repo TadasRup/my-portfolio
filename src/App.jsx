e03cfb004c4541deec98b602fc19ba7428d07df8
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-11-06 15:29:00 +0000

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import Test from "./Test"
 
 const App = () => {
   return <div>
-    <section>Hero</section>
-    <section>Parallax</section>
+    <section id="Homepage">
+      <Navbar />
+      <Hero />
+    </section>
+    <section id="Services">Parallax</section>
     <section>Services</section>
-    <section>Parallax</section>
-    <section>Portolio1</section>
-    <section>Portolio2</section>
-    <section>Portolio3</section>
-    <section>Contact</section>
+    <section id="Portfolio">Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section> 
+    {/* <Test /> */}
   </div>;
 };
 
