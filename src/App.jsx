d09d2076fe14403980c1cc25a4c9a0e75d590149
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-11-10 12:30:40 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
+
 import "./app.scss";
-import Navbar from "./components/navbar/Navbar";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
-import Parallax from "./components/parallax/Parallax"
-import Services from "./components/services/Services"
-import Test from "./Test"
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>
-    <section id="Homepage">
-      <Navbar />
-      <Hero />
-    </section>
-    <section id="Services"><Parallax type="services" /></section>
-    <section><Services /></section>
-    <section id="Portfolio"><Parallax type="portfolio" /></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="Contact">Contact</section> 
-    {/* <Test /> */}
-  </div>;
+  return (
+    <div>
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
 };
 
 export default App;
